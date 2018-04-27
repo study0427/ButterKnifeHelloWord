beb12a0bc6faf3e3678c44b7fe9da4c260aa88f7
youlingcao <youlingcao@live.cn>
2018-04-27 15:28:35 +0800

    init

---
+++ app/src/main/java/com/youlingcao/butterknife/SimpleApp.java
@@ -0,0 +1,11 @@
+package com.youlingcao.butterknife;
+
+import android.app.Application;
+import butterknife.ButterKnife;
+
+public class SimpleApp extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+    ButterKnife.setDebug(BuildConfig.DEBUG);
+  }
+}
